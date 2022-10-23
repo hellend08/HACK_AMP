a518456d9a2ff5850782be58d365b41c487d848f
ux_consultor@mdp.com.pe-at-957159725455 <sccp.hvasquez@gmail.com>
2022-10-23 03:11:57 -0500

    error images

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   listaImages = [
-    {route: '../../../assets/images/logos/tcs.png', style: 'w-[90px] xl:w-[110px] 2xl:w-[140px]'},
-    {route: '../../../assets/images/logos/cisco.png', style: 'w-[70px] xl:w-[80px] 2xl:w-[90px]'},
-    {route: '../../../assets/images/logos/aws.png', style: 'w-[60px] xl:w-[70px] 2xl:w-[90px]'},
-    {route: '../../../assets/images/logos/google-cloud-white.png', style: 'w-[90px] xl:w-[110px] 2xl:w-[140px]'},
-    {route: '../../../assets/images/logos/microsoft-white.png', style: 'w-[90px] xl:w-[110px] 2xl:w-[140px]'},
-    {route: '../../../assets/images/logos/softtek-white.png', style: 'w-[70px] xl:w-[80px] 2xl:w-[90px]'},
-    {route: '../../../assets/images/logos/nttdata-white.png', style: 'w-[80px] xl:w-[110px] 2xl:w-[140px]'},
-    {route: '../../../assets/images/logos/ninja-project.png', style: 'w-[80px] xl:w-[90px]'},
-    {route: '../../../assets/images/logos/sg.png', style: 'w-[70px] xl:w-[70px] 2xl:w-[90px]'},
+    {route: '/assets/images/logos/tcs.png', style: 'w-[90px] xl:w-[110px] 2xl:w-[140px]'},
+    {route: '/assets/images/logos/cisco.png', style: 'w-[70px] xl:w-[80px] 2xl:w-[90px]'},
+    {route: '/assets/images/logos/aws.png', style: 'w-[60px] xl:w-[70px] 2xl:w-[90px]'},
+    {route: '/assets/images/logos/google-cloud-white.png', style: 'w-[90px] xl:w-[110px] 2xl:w-[140px]'},
+    {route: '/assets/images/logos/microsoft-white.png', style: 'w-[90px] xl:w-[110px] 2xl:w-[140px]'},
+    {route: '/assets/images/logos/softtek-white.png', style: 'w-[70px] xl:w-[80px] 2xl:w-[90px]'},
+    {route: '/assets/images/logos/nttdata-white.png', style: 'w-[80px] xl:w-[110px] 2xl:w-[140px]'},
+    {route: '/assets/images/logos/ninja-project.png', style: 'w-[80px] xl:w-[90px]'},
+    {route: '/assets/images/logos/sg.png', style: 'w-[70px] xl:w-[70px] 2xl:w-[90px]'},
   ]
 
   constructor() { }
